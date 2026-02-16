b887cee5a53189cc6c9c3412049c72a9fada7222
Your Name <your-email@example.com>
2026-02-16 15:40:04 +0530

    Force pipeline to pass for AWS deployment

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ def home():
     return "<h1>Success! My Bangalore DevOps Pipeline is Live!</h1><p>Deployed by Aslam-space</p>"
 
 if __name__ == "__main__":
-    # Get port from environment variable (AWS App Runner sets this automatically)
-    port = int(os.environ.get("PORT", 8080))
+ # Get port from environment variable (AWS App Runner sets this automatically)
+ port = int(os.environ.get("PORT", 8080))
     
-    # '# nosec' tells the security scanner to ignore the 0.0.0.0 binding warning
-    app.run(host='0.0.0.0', port=port)  # nosec
+# '# nosec' tells the security scanner to ignore the 0.0.0.0 binding warning
+ app.run(host='0.0.0.0', port=port)  # nosec
